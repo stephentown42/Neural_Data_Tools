b5aa629202fd36547eed75ed06046209229db64a
stephentown42 <s.town@ucl.ac.uk>
2021-01-29 19:16:00 +0000

    updates

--- Metadata/add_electrode_position_to_block_table.py
+++ Metadata/add_electrode_position_to_block_table.py
@@ -14,6 +14,8 @@ We want to extend th
 Stephen Town - 29 Jan 2021
 '''
 
+from collections import defaultdict
+import numpy as np
 from pathlib import Path
 import pandas as pd
 
@@ -21,29 +23,15 @@ block_table_path = 'Metadata/Block_Table_2021_01_29.csv'
 e_pos_path = Path('ElectrodePositions/Data/csv')
 
 
-# Remove ferrets for which no electrode positions exist
-def filter_for_implanted_animals(bt):
-
-    acceptable_f = []                                   # List ferrets with electrode data
-
-    for pos_file in e_pos_path.glob('*.csv'):
-        acceptable_f.append( pos_file.name[1:5])
-
-    bt['f_num'] = bt['Ferret'].str[1:5]                 # Get unique identifier for each ferret
-    bt = bt[bt['f_num'].isin( acceptable_f)]            # Filter
-    
-    return bt
-
-
 def load_electrode_data(e_pos_path):    
 
-    pos_data = []                                   
+    pos_data = []                                      # Load each data frame and concatenate
 
-    for pos_file in e_pos_path.glob('*.csv'):
+    for pos_file in e_pos_path.glob('*.csv'):      
 
         pf = pd.read_csv( str(pos_file))
 
-        pf['F_num'] = int(pos_file.name[1:5])
+        pf['F_num'] = int(pos_file.name[1:5])           #<- we could also add brain region and cortical field here 
         pf['Hemisphere'] = pos_file.name[6:7]
                                 
         pos_data.append( pf)
@@ -51,38 +39,99 @@ def load_electrode_data(e_pos_path):
     pos_data = pd.concat( pos_data)
     
     pos_data['Date'] = pd.to_datetime( pos_data[['Year','Month','Day']])            # Make date one column
-    pos_data = pos_data[['F_num','Hemisphere','Channel','Date','Zero','Position']]  # Reorder 
+    pos_data = pos_data[['F_num','Hemisphere','Channel','Date','Zero','Position']]  # Reorder to make pretty
     pos_data['Depth'] = pos_data['Position'] - pos_data['Zero']                     # Recalculate depth (as may be missing in some files)
 
     return pos_data
+
+
+def get_first_movement( pos_data):
+
+    grouped = pos_data.groupby(['F_num'])
+    first_movement = defaultdict(list)
+
+    for group_name, group in grouped:
+
+        first_movement['f_num'].append( group_name)
+        first_movement['date'].append( group['Date'].min())
+        
+    first_movement['date'] = pd.to_datetime(first_movement['date'])
+
+    return pd.DataFrame.from_dict(first_movement)
     
+
+def rm_data_before_implant( block_table, first_move):
+
+    block_table['datetime'] = pd.to_datetime(block_table['datetime'])
+    block_table['f_num'] = block_table['Ferret'].str[1:5].astype('int')  # Get unique identifier for each ferret    
+
+    all_data = []
+
+    for _, row in first_move.iterrows():
+        
+        ferret_data = block_table[ block_table['f_num'] == row['f_num']]
+        ferret_data = ferret_data[ ferret_data['datetime'] > row['date']]
+        
+        all_data.append(ferret_data)
+
+    return pd.concat( all_data)
+
+
+def crossref_electrode_positions( block_table, electrode_positions, nchans=32):
+    
+    L_depth = np.zeros((block_table.shape[0], nchans))
+    R_depth = np.zeros((block_table.shape[0], nchans))
+    block_table.reset_index(inplace=True)
+
+    for idx, block in block_table.iterrows():
+
+        block_pos = electrode_positions[(electrode_positions['F_num'] == block['f_num']) & (electrode_positions['Date'] < block['datetime'])]
+
+        for chan in range(0, nchans):
+            
+            chan_pos = block_pos[(block_pos['Channel'] == chan)]
+
+            L_data = chan_pos[(chan_pos['Hemisphere'] == 'L')]
+            R_data = chan_pos[(chan_pos['Hemisphere'] == 'R')]
+
+            if L_data.shape[0] > 0:
+                L_depth[idx, chan] = L_data['Depth'].min()
+            
+            if R_data.shape[0] > 0:
+                R_depth[idx, chan] = R_data['Depth'].min()
+
+    L_depth = pd.DataFrame( np.around(L_depth, decimals=3), columns=['L'+str(x) for x in range(0, nchans)])
+    R_depth = pd.DataFrame( np.around(R_depth, decimals=3), columns=['R'+str(x) for x in range(0, nchans)])
+
+    return pd.concat([block_table, L_depth, R_depth], axis=1)
+
 
 def main():
 
 
     # Load block table
-    bt = pd.read_csv( block_table_path)
-    print(bt.shape)
+    block_table = pd.read_csv( block_table_path)
+    print(block_table.shape)
 
     # Remove duplicates (this can happen naturally as the block table is built in stages over time, collecting metadata from
     # several disks. Owing to data management requirements data sometimes migrating between disks, meaning duplicates can arise)
-    bt.drop_duplicates(inplace=True)
-    print(bt.shape)
-
-    # Remove data for animals with no electrode positions (i.e. those without moveable electrodes)
-    bt = filter_for_implanted_animals(bt)
-    print(bt.shape)
+    block_table.drop_duplicates(inplace=True)
+    block_table.drop(['DateNum','Duration'], axis=1, inplace=True)
+    print(block_table.shape)
 
     # Load electrode positions
-    e_pos = load_electrode_data(e_pos_path)
-
+    electrode_positions = load_electrode_data(e_pos_path)
+    first_move = get_first_movement(electrode_positions)
 
     # Remove data for blocks before implantation
-
-    print([e_pos.head()])
-
-
-
+    block_table = rm_data_before_implant(block_table, first_move)
+    print(block_table.shape)        
+    
+    # Cross reference electrode position
+    block_table = crossref_electrode_positions( block_table, electrode_positions)     
+    
+    block_table.drop(['index','f_num'], axis=1, inplace=True)
+    block_table.to_csv('Metadata/Block_Table_2021_01_29_extended.csv', index=False)
 
 
 if __name__ == "__main__":
